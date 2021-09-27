0a3e794358862f6aaa9a96fbe13faf2ed725267b
IELY Chabrouni <91464995+ichabrouni@users.noreply.github.com>
2021-09-27 12:35:54 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,58 @@
+<template>
+
+
+<app-navigator> </app-navigator>
+
+</template>
+
+<script>
+import {createStackNavigator} from "react-navigation-stack";
+import {createAppContainer} from "react-navigation";
+
+
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+
+import * as Location from "expo-location";
+import { Accelerometer } from "expo-sensors";
+
+
+
+
+import homescreen from "./App/screens/homescreen";
+import accelescreen from "./App/screens/accelescreen";
+import gpsscreen from "./App/screens/gpsscreen";
+import camerascreen from "./App/screens/camerascreen";
+
+
+
+
+const StackNavigator = createStackNavigator(
+ 
+ {
+    Home:homescreen,
+    Camera: camerascreen,
+    Gps: gpsscreen,
+    Accele:accelescreen,
+    
+},
+ {
+    initialRouteName: 'Home',
+  }
+)
+
+const AppNavigator = createAppContainer(StackNavigator);
+export default{
+  components: {AppNavigator},
+}
+</script>
+
+<style>
+  
+  .statusbar{
+    background-color: blue;
+    height: 50px;
+  }
+
+
+</style>
